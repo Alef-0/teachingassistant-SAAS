cb284919956daf54bc24864c2c3bbf149947986d
pauloborba <phmb@cin.ufpe.br>
2017-10-03 21:24:12 -0300

    avançando3

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -4,7 +4,7 @@ import bodyParser = require("body-parser");
 import {Aluno} from '../common/aluno';
 import {CadastroDeAlunos} from './cadastrodealunos';
 
-var taserver = express();
+var app = express();
 
 var cadastro: CadastroDeAlunos = new CadastroDeAlunos();
 
@@ -14,15 +14,15 @@ var allowCrossDomain = function(req: any, res: any, next: any) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
-taserver.use(allowCrossDomain);
+app.use(allowCrossDomain);
 
-taserver.use(bodyParser.json());
+app.use(bodyParser.json());
 
-taserver.get('/alunos', function (req: express.Request, res: express.Response) {
+app.get('/alunos', function (req: express.Request, res: express.Response) {
   res.send(JSON.stringify(cadastro.getAlunos()));
 })
 
-taserver.post('/aluno', function (req: express.Request, res: express.Response) {
+app.post('/aluno', function (req: express.Request, res: express.Response) {
   var aluno: Aluno = <Aluno> req.body; //verificar se � mesmo Aluno!
   aluno = cadastro.cadastrar(aluno);
   if (aluno) {
@@ -32,7 +32,7 @@ taserver.post('/aluno', function (req: express.Request, res: express.Response) {
   }
 })
 
-taserver.put('/aluno', function (req: express.Request, res: express.Response) {
+app.put('/aluno', function (req: express.Request, res: express.Response) {
   var aluno: Aluno = <Aluno> req.body;
   aluno = cadastro.atualizar(aluno);
   if (aluno) {
@@ -42,6 +42,12 @@ taserver.put('/aluno', function (req: express.Request, res: express.Response) {
   }
 })
 
-taserver.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+function closeServer(): void {
+   server.close();
+}
+
+export { app, server, closeServer }
