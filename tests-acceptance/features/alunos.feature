ec8b32b87a1cd26efdde95bb98ef3baca65fd174
Alef-0 <alefgabryel123@gmail.com>
2022-09-18 19:55:09 -0300

    DEU CERTO - Remoção

--- tests-acceptance/features/alunos.feature
+++ tests-acceptance/features/alunos.feature
@@ -8,8 +8,8 @@ Given I cannot see a student with CPF "683" in the students list
 When I try to register the student "Paulo" with CPF "683"
 Then I can see "Paulo" with CPF "683" in the students list
 
-# Scenario: Removing Student from list
-# Given I am at the students page
-# Given I can see a student with name "Mari" and CPF "683" in the students list
-# When I select the remove button
-# Then I shouldn't see "Mari" and CPF "683" in the students list
+Scenario: Removing Student from list
+Given I am at the students page
+Given I can see a student with name "Mari" and CPF "321" in the students list
+When I select the remove button on the student with name "Mari" and CPF "321" in the students list
+Then I shouldn't see "Mari" and CPF "321" in the students list
--- tests-acceptance/stepdefinitions/alunos.ts
+++ tests-acceptance/stepdefinitions/alunos.ts
@@ -8,6 +8,21 @@ let expect = chai.expect;
 let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
 let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
 
+async function createStudent(name, cpf){
+    await $("input[name='namebox']").sendKeys(<string> name);
+    await $("input[name='cpfbox']").sendKeys(<string> cpf);
+    await element(by.className('criar')).click();
+}
+
+async function find(name, cpf, quantity){
+    var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+    await allalunos;
+    var samenamecpf = allalunos.filter(elem => sameCPF(elem,cpf) && sameName(elem,name));
+    await samenamecpf;
+    await samenamecpf.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(quantity));
+}
+
+
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the students page$/, async () => {
         await browser.get("http://localhost:4200/");
@@ -24,17 +39,28 @@ defineSupportCode(function ({ Given, When, Then }) {
         await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     });
 
+    Given(/^I can see a student with name "([^\"]*)" and CPF "(\d*)" in the students list$/, async (name, cpf) => {
+        await createStudent(name,cpf);
+        await find(name,cpf, 1);
+    });
+
     When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-        await $("input[name='namebox']").sendKeys(<string> name);
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await element(by.buttonText('Adicionar')).click();
+        await createStudent(name,cpf)
     });
 
     Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+        await find(name,cpf, 1);
+    });
+
+    When(/^I select the remove button on the student with name "([^\"]*)" and CPF "(\d*)" in the students list$/, async (name,cpf) =>{
         var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
         await allalunos;
-        var samenamecpf = allalunos.filter(elem => sameCPF(elem,cpf) && sameName(elem,name));
-        await samenamecpf;
-        await samenamecpf.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-    });
+        await allalunos.filter(elem => 
+            sameCPF(elem,cpf)
+        ).first().element(by.className('excluir')).click();
+    })
+
+    Then(/^I shouldn't see "([^\"]*)" and CPF "(\d*)" in the students list$/, async(name,cpf)=>{
+        await find(name,cpf,0);
+    })
 })
